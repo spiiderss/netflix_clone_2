a2c9160d266e7c45da887f8b3ae434cb8c4a37ae
spiderss <945792171@qq.com>
2022-04-23 09:53:21 +0800

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
+const intercept = require("intercept-stdout");
+
+// 安全的移除recoil 重复key 的警告
+function interceptStdout(text) {
+  if (text.includes("Duplicate atom key")) {
+    return "";
+  }
+  return text;
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+intercept(interceptStdout);
+
+module.exports = nextConfig;
